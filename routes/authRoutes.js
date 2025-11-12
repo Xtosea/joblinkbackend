cdd5925a9864882dba05d31df4a71e6835c9dc88
Chris <106016563+Xtosea@users.noreply.github.com>
2025-11-12 08:17:25 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,16 +1,5 @@
-import express import express from "express";
-import { loginUser, registerUser } from "../controllers/authController.js";
-
-const router = express.Router();
-
-// ✅ POST /api/auth/register
-router.post("/register", registerUser);
-
-// ✅ POST /api/auth/login
-router.post("/login", loginUser);
-
-export default router;
- "express";
+// routes/authRoutes.js
+import express from "express";
 import { loginUser, registerUser } from "../controllers/authController.js";
 
 const router = express.Router();
