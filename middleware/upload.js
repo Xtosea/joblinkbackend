fff6399b3129a51c3dd888b6a13c63b0a1350f78
Chris <106016563+Xtosea@users.noreply.github.com>
2026-01-06 04:46:29 +0100

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,4 +1,9 @@
-// middlewares/upload.js
 import multer from "multer";
 
-export const upload = multer({ storage: multer.memoryStorage() });
+export const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB
+});
+
+console.log("UPLOAD ID:", req.params.id);
+console.log("FILES:", req.files);
