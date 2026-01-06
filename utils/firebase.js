473f1d51b08c1445fe6c146ba0cf1a444f0b7c0a
Chris <106016563+Xtosea@users.noreply.github.com>
2026-01-06 04:52:21 +0100

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp, cert } from "firebase-admin/app";
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
 
 const serviceAccountBase64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
@@ -8,15 +8,16 @@ if (!serviceAccountBase64 || !bucketName) {
   throw new Error("Missing Firebase env variables");
 }
 
-// Decode service account
 const serviceAccount = JSON.parse(
   Buffer.from(serviceAccountBase64, "base64").toString("utf-8")
 );
 
-// Initialize Firebase admin
-initializeApp({
-  credential: cert(serviceAccount),
-  storageBucket: bucketName,
-});
+// Prevent re-initialization on hot reloads
+if (!getApps().length) {
+  initializeApp({
+    credential: cert(serviceAccount),
+    storageBucket: bucketName,
+  });
+}
 
 export const bucket = getStorage().bucket();
