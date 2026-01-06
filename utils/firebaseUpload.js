9aa93de39a9ea256f8a06eba4f1dd5fecb0d2863
Chris <106016563+Xtosea@users.noreply.github.com>
2026-01-06 04:53:43 +0100

    Update firebaseUpload.js

--- utils/firebaseUpload.js
+++ utils/firebaseUpload.js
@@ -1,12 +1,6 @@
-import multer from "multer";
 import { bucket } from "./firebase.js";
 import { v4 as uuidv4 } from "uuid";
 
-// Store files in memory (required for Firebase)
-const storage = multer.memoryStorage();
-export const upload = multer({ storage });
-
-// Upload file to Firebase Storage
 export const uploadFileToFirebase = async (file, folder = "uploads") => {
   if (!file) return null;
 
