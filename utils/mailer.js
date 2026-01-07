1c2229e51be2d7032b0dcfa8db34f9ead866a5f1
Chris <106016563+Xtosea@users.noreply.github.com>
2026-01-07 19:08:55 +0100

    Create mailer.js

---
+++ utils/mailer.js
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS, // Gmail App Password
+  },
+});
+
+export const sendApplicationEmail = async ({ to, fullname, link }) => {
+  return transporter.sendMail({
+    from: `"JobLink" <${process.env.EMAIL_USER}>`,
+    to,
+    subject: "Application Received – Next Steps",
+    html: `
+      <p>Hello ${fullname},</p>
+      <p>Your application was received successfully.</p>
+      <p>Please upload your proof & CV using the link below:</p>
+      <p><a href="${link}">${link}</a></p>
+      <p><b>Note:</b> This link expires in 48 hours.</p>
+    `,
+  });
+};
