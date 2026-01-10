8df4810f48ad9358e332d04da119f4c43dcc542b
Chris <106016563+Xtosea@users.noreply.github.com>
2026-01-10 12:48:20 +0100

    Update Application.js

--- models/Application.js
+++ models/Application.js
@@ -5,7 +5,11 @@ const applicationSchema = new mongoose.Schema(
     fullname: { type: String, required: true, trim: true },
     email: { type: String, required: true, trim: true },
     mobile: { type: String, required: true, trim: true },
-    jobType: { type: String, enum: ["Full-time", "Part-time"], required: true },
+    jobType: {
+      type: String,
+      enum: ["Full-time", "Part-time"],
+      required: true,
+    },
     jobPosition: { type: String, required: true, trim: true },
 
     proofFile: String,
@@ -18,6 +22,7 @@ const applicationSchema = new mongoose.Schema(
       default: "Pending",
     },
 
+    // 🔑 Email access token
     emailToken: { type: String, required: true, unique: true },
     tokenExpiresAt: { type: Date, required: true },
   },
