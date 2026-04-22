ebf28adf93aeea2a2bb189e4d61238f61d728f1a
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-22 16:17:07 +0100

    Create Notification.js

---
+++ models/Notification.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema({
+  message: String,
+  type: {
+    type: String,
+    default: "info",
+  },
+
+  read: {
+    type: Boolean,
+    default: false,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Notification", notificationSchema);
