07fc445bf7cd040ff1955dfcced4a17cb154dd09
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-23 22:52:15 +0100

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -40,7 +40,7 @@ export const sendApplicationNotification = async ({ email, fullname, link }) => 
       <p>Important Notes:</p>
       <ul>
         <li>Each applicant is entitled to up to 3 interview opportunities within a 3-month support period.
-If you successfully pass any of the interviews, you will receive a congratulations message, and your application process with JobLinks will be completed.
+If you successfully pass any of the interviews, you will receive a congratulations message, and your application process with JobLink will be completed.
 If you are not successful after 3 interview attempts within the 3-month period, you may reapply and continue with a new application and payment.
 Please note that JobLink does not guarantee employment. However, we prepaid your CV and give you full interview training, and guidance to help improve your chances of securing a job.
 If you have any questions, feel free to contact us. </ul>
