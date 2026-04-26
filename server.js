ba15116da758bde646bba3f5c08a0eb8312904d7
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-26 15:25:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import authRoutes from "./routes/authRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import notificationRoutes from "./routes/notificationRoutes.js";
 import jobRoutes from "./routes/jobRoutes.js";
-import JobAuthRoutes from "./routes/JobAuthRoute.js";
+import postAuthRoutes from "./routes/postAuthRoute.js";
 
 dotenv.config();
 const app = express();
