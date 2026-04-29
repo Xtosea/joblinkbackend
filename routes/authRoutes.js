b0b1d2368b4142e000ec8d4a5dbb52a92929aca9
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-29 18:59:58 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { adminLogin } from "../controllers/adminController.js";
+import { login, register } from "../controllers/auth.js";
 
 const router = express.Router();
 
