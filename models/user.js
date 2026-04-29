ff58fa3429b2fc3257376d69d1aa8bfec5d8fcf4
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-29 16:53:43 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,11 +2,41 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    fullname: { type: String, required: true, trim: true },
-    email: { type: String, required: true, unique: true, trim: true },
-    mobile: { type: String, trim: true }, // optional for admins
-    password: { type: String, required: true },
-    role: { type: String, enum: ["admin", "applicant"], default: "applicant" },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    mobile: {
+      type: String,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "applicant", "employer"],
+      default: "applicant",
+    },
+
+    savedJobs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+      },
+    ],
   },
   { timestamps: true }
 );
